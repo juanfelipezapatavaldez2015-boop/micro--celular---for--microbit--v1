04c987dbe73903e78de82c805daefaeb727782bd
juanfelipezapatavaldez2015-boop <juanfelipezapatavaldez2015@gmail.com>
2026-04-07 15:06:44 -0500

    errores corregidos

--- main.ts
+++ main.ts
@@ -1,76 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . # . .
-        . . # . .
-        # . # . #
-        # . . . #
-        # # # # #
-        `)
-    kittenwifi.ntp_get(kittenwifi.NtpTimeType.SS)
-    kittenwifi.wifi_join("FLIAZAPATA-ULTRA", "Viviz@pata1329")
-    kittenwifi.rest_header(kittenwifi.HeaderType.ContentType, "application/json")
-    kittenwifi.rest_header(kittenwifi.HeaderType.ContentType, "processor mediatek 1.0 horizon beta")
-    kittenwifi.rest_header(kittenwifi.HeaderType.UserAgent, "Mozilla 5.0")
-    kittenwifi.mqtt_subscribe("/hello", 0)
-    kittenwifi.rest_request("GET", "www.google.com")
-    kittenwifi.rest_request("GET", "/api/test?apple=1")
-    kittenwifi.mqtt_publish(
-    "/Api",
-    "token_auth_3af892"
-    )
-    kittenwifi.wifi_init_pw(kittenwifi.SerialPorts.PORT1)
-    kittenwifi.mqtt_sethost_auth(
-    "iot.kittenbot.cn",
-    "node01",
-    "I:MICRO",
-    "1253"
-    )
-    kittenwifi.mqtt_sethost_auth_port(
-    "iot.kittenbot.cn",
-    1883,
-    "I:MICRO",
-    "MICRO:CELL",
-    "1253"
-    )
-    kittenwifi.mqtt_sethost_port("iot.kittenbot.cn", 1883, "I:MICRO")
-    kittenwifi.rest_host("iot.kittenbot.cn", 80, 443)
-    kittenwifi.ntp_get(kittenwifi.NtpTimeType.s1970)
-    kittenwifi.mqtt_sethost("iot.kittenbot.cn", "node01")
-    kittenwifi.mqtt_sethost("iot.kittenbot.cn", "node01")
-    kittenwifi.mqtt_sethost("iot.kittenbot.cn", "node01")
-    kittenwifi.rest_request("GET", "/api/test?apple=1")
-    kittenwifi.udp_comm("192.168.18.1", 1)
-    kittenwifi.udp_comm("192.168.18.1", 1)
-    kittenwifi.udp_send("Wifi port1")
-    kittenwifi.udp_send("Micro:bit connected")
-    kittenwifi.wifi_changename("MICRO:CELL")
-    kittenwifi.mqtt_publish(
-    "/Api",
-    "token_auth_3af892",
-    1,
-    1
-    )
-    if (true) {
-        basic.showIcon(IconNames.Yes)
-    } else {
-        basic.showIcon(IconNames.No)
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 4; index++) {
-        radio.setFrequencyBand(53)
-        radio.setTransmitPower(7)
-        radio.setGroup(1)
-        radio.setTransmitSerialNumber(true)
-        radio.sendNumber(12)
-        radio.sendValue("Sim Card", 0)
-        radio.sendValue("Movistar", 12)
-        radio.sendString("telefono movil")
-    }
-    if (true) {
+    if (SElinux == 0) {
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -78,23 +7,131 @@ input.onButtonPressed(Button.B, function () {
             # . . . #
             # # # # #
             `)
+        kittenwifi.ntp_get(kittenwifi.NtpTimeType.SS)
+        kittenwifi.wifi_join("FLIAZAPATA-ULTRA", "Viviz@pata1329")
+        kittenwifi.rest_header(kittenwifi.HeaderType.ContentType, "application/json")
+        kittenwifi.rest_header(kittenwifi.HeaderType.ContentType, "processor mediatek 1.0 horizon beta")
+        kittenwifi.rest_header(kittenwifi.HeaderType.UserAgent, "Mozilla 5.0")
+        kittenwifi.wifi_changename("I:micro device")
+        if (true) {
+            kittenwifi.udp_send("Micro:bit connected")
+        } else {
+            basic.showLeds(`
+                # # # # .
+                # # # # #
+                # # . . .
+                # # # # .
+                . . . . .
+                `)
+            control.reset()
+        }
+        kittenwifi.mqtt_publish(
+        "shell console",
+        "'OR 1=1'-- o #"
+        )
+        kittenwifi.mqtt_sethost_auth(
+        "iot.kittenbot.cn",
+        "node01",
+        "'OR 1=1'-- o #",
+        ""
+        )
+        kittenwifi.mqtt_sethost_port("iot.kittenbot.cn", 1883, "00000009")
+        kittenwifi.rest_host("iot.kittenbot.cn", 80, 443)
+        kittenwifi.mqtt_sethost("iot.kittenbot.cn", "node01")
+        kittenwifi.mqtt_sethost("iot.kittenbot.cn", "node01")
+        kittenwifi.rest_request("GET", "/api/test?apple=1")
+        kittenwifi.udp_comm("192.168.18.1", 1)
+        kittenwifi.udp_comm("192.168.18.1", 4)
+        if (true) {
+            basic.showIcon(IconNames.Yes)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
     } else {
         basic.showLeds(`
             . . # . .
-            . # . # .
-            # . # . #
-            # . # . #
-            # # # # #
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
             `)
+        control.reset()
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    if (Sistema_Operativo == 25) {
+        if (Firmware_version == 4) {
+            basic.showString("4")
+            basic.pause(1000)
+            control.reset()
+        } else {
+            serial.redirectToUSB()
+            serial.redirect(
+            SerialPin.P0,
+            SerialPin.P1,
+            BaudRate.BaudRate115200
+            )
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                # . . . #
+                # # # # #
+                `)
+        }
+    } else {
+        control.reset()
+        serial.redirectToUSB()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (SElinux == 0) {
+        for (let index = 0; index < 4; index++) {
+            radio.setFrequencyBand(53)
+            radio.setTransmitPower(7)
+            radio.setGroup(1)
+            radio.setTransmitSerialNumber(true)
+            radio.sendNumber(12)
+            radio.sendValue("baseband", 9856810520262024)
+            radio.sendValue("device", 12)
+            radio.sendString("telefono movil")
+        }
+        if (true) {
+            basic.showLeds(`
+                . . # . .
+                . # . # .
+                # . # . #
+                # . # . #
+                # # # # #
+                `)
+        } else {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                # . . . #
+                # # # # #
+                `)
+        }
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
+        control.reset()
+    }
+})
+let Sistema_Operativo = 0
+let SElinux = 0
+let Firmware_version = 0
 let bootloader_security_checksum = 0
 bootloader_security_checksum += 1
-serial.redirect(
-SerialPin.P0,
-SerialPin.P1,
-BaudRate.BaudRate115200
-)
+Firmware_version += 4
+SElinux += 0
+Sistema_Operativo += 25
 kittenwifi.ntp_get(kittenwifi.NtpTimeType.s1970)
 basic.pause(3000)
 if (bootloader_security_checksum == 1) {
@@ -170,3 +207,11 @@ basic.showLeds(`
     . . . # .
     . . # . .
     `)
+radio.setGroup(1)
+radio.setTransmitPower(7)
+radio.setFrequencyBand(56)
+radio.raiseEvent(
+EventBusSource.MICROBIT_ID_RADIO,
+EventBusValue.MICROBIT_EVT_ANY
+)
+radio.setTransmitSerialNumber(true)
