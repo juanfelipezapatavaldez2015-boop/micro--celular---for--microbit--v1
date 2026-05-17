d2a50f5add7881c2a2f85d950c73012787aab878
juanfelipezapatavaldez2015-boop <juanfelipezapatavaldez2015@gmail.com>
2026-05-16 20:06:46 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,7 +15,6 @@ radio.onReceivedNumber(function (receivedNumber) {
         `)
 })
 function bootkitharddiskvolume3 (text: string) {
-    bootloader_security_checksum += control.deviceSerialNumber()
     if (true) {
         serial.redirectToUSB()
         serial.redirect(
@@ -25,96 +24,116 @@ function bootkitharddiskvolume3 (text: string) {
         )
     }
 }
+function bootkitharddiskvolume1 (bool: boolean, text: string) {
+    bootloader_security_checksum += control.deviceSerialNumber()
+}
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 4; index++) {
-        basic.showLeds(`
-            . . # . .
-            . . . . .
-            # . . . #
-            # . . . #
-            # # # # #
-            `)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . . . #
-            # . . . #
-            # # # # #
-            `)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            # . . . #
-            # # # # #
-            `)
-    }
-    kittenwifi.wifi_join("FLIAZAPATA-ULTRA", "Viviz@pata1329")
-    kittenwifi.udp_comm(kittenwifi.wifi_addr(), 1234)
-    kittenwifi.udp_send("handshake exitoso")
-    kittenwifi.ntp_get(kittenwifi.NtpTimeType.SS)
-    kittenwifi.wifi_changename("Idevice:bit")
-    kittenwifi.rest_header(kittenwifi.HeaderType.ContentType, "Authme 5.7.0 .jar")
-    kittenwifi.rest_header(kittenwifi.HeaderType.ContentType, "Micro Bit processor")
-    kittenwifi.rest_header(kittenwifi.HeaderType.UserAgent, "Mozilla 5.0")
-    kittenwifi.rest_request("GET", "/api/test?apple=1")
-    kittenwifi.rest_host("iot.kittenbot.cn", 80)
-    kittenwifi.mqtt_sethost("iot.kittenbot.cn", "node01")
-    kittenwifi.rest_host("iot.kittenbot.cn", 80, 443)
-    kittenwifi.rest_request("GET", "/api/test?apple=1")
-    kittenwifi.mqtt_sethost("iot.kittenbot.cn", "node01")
-    kittenwifi.mqtt_subscribe_basic("iot.kittenbot.cn")
-    kittenwifi.mqtt_publish_basic("iot.kittenbot.cn", "'OR 1=1'-- o #")
-    kittenwifi.mqtt_publish(
-    "/console",
-    "'OR 1=1'-- o #"
-    )
-    kittenwifi.mqtt_sethost("iot.kittenbot.cn", "node01")
-    if (true) {
-        basic.showIcon(IconNames.Yes)
-    } else {
-        basic.showIcon(IconNames.No)
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    if (Firmware_version == 12) {
-        basic.showString("12")
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    while (true) {
-        basic.showLeds(`
-            . . . . .
-            # # . . .
-            # . # # #
-            # # . # #
-            . . . . .
-            `)
-        radio.setGroup(1)
-        radio.setTransmitPower(7)
-        radio.setFrequencyBand(40)
-        radio.setTransmitSerialNumber(true)
-        radio.sendValue("KEY_ID", randint(1, 4.845485456e+161))
-        radio.sendString("KEY_ID")
-        radio.raiseEvent(
-        EventBusSource.MES_SIGNAL_STRENGTH_ID,
-        EventBusValue.MICROBIT_EVT_ANY
-        )
-        if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= 5) {
+    bootkitharddiskvolume4application1wifi()
+    if (TPM_20 == control.deviceName()) {
+        for (let index = 0; index < 4; index++) {
             basic.showLeds(`
                 . . # . .
-                . # . # .
-                . . . # .
-                . # # # .
-                . # # # .
+                . . . . .
+                # . . . #
+                # . . . #
+                # # # # #
                 `)
-            break;
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . . . #
+                # . . . #
+                # # # # #
+                `)
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                # . . . #
+                # # # # #
+                `)
+        }
+        kittenwifi.wifi_join("FLIAZAPATA-ULTRA", "Viviz@pata1329")
+        kittenwifi.udp_comm(kittenwifi.wifi_addr(), 1234)
+        kittenwifi.udp_send("handshake exitoso")
+        kittenwifi.ntp_get(kittenwifi.NtpTimeType.SS)
+        kittenwifi.wifi_changename("Idevice:bit")
+        kittenwifi.rest_header(kittenwifi.HeaderType.ContentType, "Authme 5.7.0 .jar")
+        kittenwifi.rest_header(kittenwifi.HeaderType.ContentType, "Micro Bit processor")
+        kittenwifi.rest_header(kittenwifi.HeaderType.UserAgent, "Mozilla 5.0")
+        kittenwifi.rest_request("GET", "/api/test?apple=1")
+        kittenwifi.rest_host("iot.kittenbot.cn", 80)
+        kittenwifi.mqtt_sethost("iot.kittenbot.cn", "node01")
+        kittenwifi.rest_host("iot.kittenbot.cn", 80, 443)
+        kittenwifi.rest_request("GET", "/api/test?apple=1")
+        kittenwifi.mqtt_sethost("iot.kittenbot.cn", "node01")
+        kittenwifi.mqtt_subscribe_basic("iot.kittenbot.cn")
+        kittenwifi.mqtt_publish_basic("iot.kittenbot.cn", "'OR 1=1'-- o #")
+        kittenwifi.mqtt_publish(
+        "/console",
+        "'OR 1=1'-- o #"
+        )
+        kittenwifi.mqtt_sethost("iot.kittenbot.cn", "node01")
+        if (true) {
+            basic.showIcon(IconNames.Yes)
         } else {
             basic.showIcon(IconNames.No)
         }
     }
 })
+function bootkitharddiskvolume4application1wifi () {
+    TPM_20 = control.deviceName()
+}
+function bootkitharddiskvolume2 (num: number, num2: number, text: string, text2: string) {
+    Firmware_version += 12
+}
+input.onButtonPressed(Button.AB, function () {
+    bootkitharddiskvolume2(1, 2, "version", "sistema")
+    if (Firmware_version == 12) {
+        basic.showString("12")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    bootkitharddiskvolume5application2radioemisor()
+    if (TPM_20 == control.deviceName()) {
+        while (true) {
+            basic.showLeds(`
+                . . . . .
+                # # . . .
+                # . # # #
+                # # . # #
+                . . . . .
+                `)
+            radio.setGroup(1)
+            radio.setTransmitPower(7)
+            radio.setFrequencyBand(40)
+            radio.setTransmitSerialNumber(true)
+            radio.sendValue("KEY_ID", randint(1, 4.845485456e+161))
+            radio.sendString("KEY_ID")
+            radio.raiseEvent(
+            EventBusSource.MES_SIGNAL_STRENGTH_ID,
+            EventBusValue.MICROBIT_EVT_ANY
+            )
+            if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= 5) {
+                basic.showLeds(`
+                    . . # . .
+                    . # . # .
+                    . . . # .
+                    . # # # .
+                    . # # # .
+                    `)
+                break;
+            } else {
+                basic.showIcon(IconNames.No)
+            }
+        }
+    }
+})
+function bootkitharddiskvolume5application2radioemisor () {
+    TPM_20 = control.deviceName()
+}
 let Firmware_version = 0
+let TPM_20 = ""
 let bootloader_security_checksum = 0
 basic.showLeds(`
     . # . . .
@@ -123,9 +142,9 @@ basic.showLeds(`
     . # # . .
     . # # # .
     `)
-bootloader_security_checksum += control.deviceSerialNumber()
-bootkitharddiskvolume3(serial.readUntil(serial.delimiters(Delimiters.Hash)))
-Firmware_version += 12
+bootkitharddiskvolume1(true, "bootloader")
+bootkitharddiskvolume2(1, 2, "version", "sistema")
+bootkitharddiskvolume3("recovery")
 if (bootloader_security_checksum == control.deviceSerialNumber()) {
     basic.pause(200)
     basic.showLeds(`
@@ -156,13 +175,7 @@ if (bootloader_security_checksum == control.deviceSerialNumber()) {
     }
 }
 basic.forever(function () {
-	
-})
-basic.forever(function () {
     if (input.temperature() > 38) {
         basic.clearScreen()
     }
-})
-control.inBackground(function () {
-	
 })
